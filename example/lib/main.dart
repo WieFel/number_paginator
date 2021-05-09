e57145e22d7ef872f96ebc04d02774f05a9ae0d8
Felix <felix.wielander@gmail.com>
2021-05-09 14:15:46 +0200

    Created basic working example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,26 +3,50 @@ import 'package:number_paginator/number_paginator.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _numPages = 10;
+  List<Widget> _pages;
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pages = List.generate(
+      _numPages,
+      (index) => Center(
+        child: Text("Page ${index + 1}"),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Backdrop Demo',
       home: Scaffold(
+        appBar: AppBar(
+          title: Text("Number Paginator Demo"),
+        ),
         body: Column(
           children: [
             Expanded(
               child: Container(
-                color: Colors.orange,
+                color: Theme.of(context).backgroundColor,
+                child: _pages[_currentPage],
               ),
             ),
-            Container(
-              margin: EdgeInsets.only(bottom: 32),
-              child: NumberPaginator(
-                pageOptionsShown: 5,
-                onPageChange: (index) => print("Page selected $index"),
-                numberPages: 10,
-              ),
+            NumberPaginator(
+              pageOptionsShown: 4,
+              numberPages: _numPages,
+              onPageChange: (int index) {
+                setState(() {
+                  _currentPage = index - 1;
+                });
+              },
             ),
           ],
         ),
