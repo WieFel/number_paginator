44a424a812d0d5a4c7d964442b3f206e441a525f
Felix <felix.wielander@gmail.com>
2022-09-08 14:59:07 +0200

    Export controller to expose it to the public

--- lib/number_paginator.dart
+++ lib/number_paginator.dart
@@ -2,4 +2,5 @@ library number_paginator;
 
 export 'src/model/config.dart';
 export 'src/model/display_mode.dart';
+export 'src/ui/number_paginator_controller.dart';
 export 'src/ui/number_paginator.dart';
