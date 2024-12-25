b213098f936863483a6e2e012fdeda81a28b4724
Felix <felix.wielander@gmail.com>
2024-12-25 11:12:29 +0100

    Upgrade to Flutter 3.27.0 and other upgrades (#23)
    * Upgrade Flutter to 3.27.1
    
    * Upgrade gradle
    
    * Raise language constraint
    
    * Improve example/main.dart
    
    * Adapt colors
    
    * Re-create android/ folder
    
    * New top screenshots
    
    * Update screenshots in readme
    
    * Upgrade package major versions
    
    * iOS upgrades

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,30 +13,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.from(
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+        ),
       ),
-      home: DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text("Number Paginator"),
-            bottom: const TabBar(
-              tabs: [
-                Tab(child: Text("Hidden")),
-                Tab(child: Text("Numbers")),
-                Tab(child: Text("Dropdown")),
-                Tab(child: Text("Custom Builder")),
-              ],
-            ),
-          ),
-          body: const TabBarView(
-            children: [
-              OnlyArrowsPage(),
-              NumbersPage(),
-              DropdownPage(),
-              BuilderPage(),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("Number Paginator"),
+          bottom: const TabBar(
+            tabs: [
+              Tab(child: Text("Hidden")),
+              Tab(child: Text("Numbers")),
+              Tab(child: Text("Dropdown")),
+              Tab(child: Text("Custom Builder")),
             ],
           ),
+        ),
+        body: const TabBarView(
+          children: [
+            OnlyArrowsPage(),
+            NumbersPage(),
+            DropdownPage(),
+            BuilderPage(),
+          ],
         ),
       ),
     );
